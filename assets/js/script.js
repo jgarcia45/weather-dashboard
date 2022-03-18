941f0fc1fa94e1b4d194d3aaeae3605085174fdd
Juan <jgarcia1545@gmail.com>
2022-03-18 16:03:13 -0700

    Created function for current forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,97 @@
 // API Key
 var apiKey = "d34c5a7f5b829f5df306f1143caac581";
 
-// Displays the Weather as of Now
-var currentWeather = function () {
-    var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=alerts&appid="+apiKey;
+// Variable Definitions
+var currentTemperature = $('#currentWeather');
 
-        fetch(apiURL).then(function (response) {
-            if (response.ok) {
-                response.json().then(function (data) {
-                    console.log(data);
+// Displays the Weather as of Now
+var currentWeather = function (city) {
+    var cityAPI = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
+
+    // Checks City API
+    fetch(cityAPI).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                var latitude = data[0].lat;
+                var longitude = data[0].lon;
+                //console.log(data);
+
+                // City Name
+                var name = $('<h3></h3>').text(data[0].name);
+                $('#currentWeather').append(name);
+
+                // Checks Lat & Lon API
+                var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=alerts&units=imperial&appid=" + apiKey;
+
+                fetch(apiURL).then(function (response) {
+                    if (response.ok) {
+                        response.json().then(function (data) {
+                            //console.log(data);
+                            // Temperature
+                            var temp = $('<p></p>').text("Temp: " + data.current.temp);
+                            $('#currentWeather').append(temp);
+                            // Wind
+                            var wind = $('<p></p>').text("Wind: " + data.current.wind_speed);
+                            $('#currentWeather').append(wind);
+                            // Humidity
+                            var humidity = $('<p></p>').text("Humidity: " + data.current.humidity);
+                            $('#currentWeather').append(humidity);
+                            // UV Index
+                            var uvi = $('<p></p>').text("Wind: " + data.current.uvi);
+                            $('#currentWeather').append(uvi);
+                        });
+                    }
+                }).catch(function (error) {
+                    alert("Unable to connect to Open Weather");
                 });
-            }
-        }).catch(function (error) {
-            alert("Unable to connect to Open Weather");
-        });
+            });
+        }
+    }).catch(function (error) {
+        alert("Unable to connect to Open Weather");
+    });
+
 };
 
-currentWeather();
+var futureWeather = function (city) {
+    var cityAPI = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
+
+    // Checks City API
+    fetch(cityAPI).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                var latitude = data[0].lat;
+                var longitude = data[0].lon;
+                //console.log(data);
+
+                // Checks Lat & Lon API
+                var futureForecast = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=current,minutely,hourly,alerts&units=imperial&appid=" + apiKey;
+
+                fetch(futureForecast).then(function (response) {
+                    if (response.ok) {
+                        response.json().then(function (data) {
+                            console.log(data);
+                            for (var i = 0; i < 5; i++) {
+                                console.log(data.daily[i].dt);
+                                var date = new Date(data.daily[i].dt);
+                            }
+                        });
+                    }
+                }).catch(function (error) {
+                    alert("Unable to connect to Open Weather");
+                });
+            });
+        }
+    }).catch(function (error) {
+        alert("Unable to connect to Open Weather");
+    });
+};
+
+// Button Event Listener
+$("#searchButton").on("click", function (event) {
+    event.preventDefault();
+
+    var city = $("#cityName").val();
+    currentWeather(city);
+    futureWeather(city);
+
+});
